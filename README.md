80197cc365468059400d5f2d83c8b362bf020fa2
Paweł Palacz <ppoz21@MacBook-Pro-Pawe.local>
2019-11-14 19:19:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python-od-podstaw
---
+++ spotkanie 1/lekcja 1.py
@@ -0,0 +1,19 @@
+# Komentarz jednolinijkowy
+
+"""
+To jest komentarz
+który ma wiele linijek
+"""
+
+# Definiowanie zmiennych
+
+x = 1
+y = "1"
+
+# Operacjie I/O (Input/Output)
+
+print("Out")
+z = int(input("Podaj liczbe: "))
+print("Podałeś liczbę: ", z)
+
+input("Aby zakończyc program naciśnij ENTER")
